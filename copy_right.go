b6bdd5a3e747a867c2157ebb90712a5eb07e978c
Ally Dale <vipally@163.com>
2016-09-12 06:45:10 +0800

    Add cmdline refer

--- copy_right.go
+++ copy_right.go
@@ -1,15 +1,19 @@
 package cpright
 
+import (
+	"github.com/vipally/cmdline"
+)
+
 var (
-	copyRight = `CopyRight @Ally Dale(vipally@gmail.com)
-Author	: Ally Dale(vipally@gmail.com)
-Blog	: http://blog.csdn.net/vipally
-Site	: https://github.com/vipally
+	copyRight = `CopyRight @Ally Dale(vipally@gmail.com) 2016
+    Author  : Ally Dale(vipally@gmail.com)
+    Blog    : http://blog.csdn.net/vipally
+    Site    : https://github.com/vipally
 `
 )
 
-func init() {
-
+func init() { //set copyright for vipally
+	cmdline.CopyRight(copyRight)
 }
 
 //get the CopyRight declaration for vipally
