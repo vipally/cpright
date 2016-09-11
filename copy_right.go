b3ec676f4e31763b995bc86c4f76770cb90abe2b
Ally Dale <vipally@163.com>
2016-09-12 06:19:54 +0800

    add copyright file

---
+++ copy_right.go
@@ -0,0 +1,18 @@
+package cpright
+
+var (
+	copyRight = `CopyRight @Ally Dale(vipally@gmail.com)
+Author	: Ally Dale(vipally@gmail.com)
+Blog	: http://bloc/csdn.net/vipally
+Site	: https://github.com/vipally
+`
+)
+
+func init() {
+
+}
+
+//get the CopyRight declare for vipally
+func CopyRight() string {
+	return copyRight
+}
